83a19e091b15ea858b6f59019c002cd06d1eda94
Jake Lucas <jak11772@esri.com>
2021-12-07 08:35:59 -0500

    getting all trials

--- src/nciRetriever/__main__.py
+++ src/nciRetriever/__main__.py
@@ -2,9 +2,14 @@ import requests
 import logging
 from urllib.parse import urljoin
 import json
+import time
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 def main():
 
@@ -19,17 +24,44 @@ def main():
     trialEndpoint = urljoin(baseUrl, 'trials')
     logger.debug(trialEndpoint)
 
-    payload = {
-        
-    }
-
-
-    trialsResponse = requests.get(trialEndpoint, headers=headers, params=payload)
+    trialsResponse = requests.get(trialEndpoint, headers=headers)
     trialsResponse.raise_for_status()
     trialJson = trialsResponse.json()
+    totalNumTrials = trialJson['total']
+    finalFileNumTrials = totalNumTrials % 50
+
+    totalTrialsData = []
+
+    start = time.perf_counter()
+
+    for trialNum in range(0, totalNumTrials, 50):
+        logger.debug(trialNum)
+        payload = {
+            'size': 50,
+            'from': trialNum
+        }
+
+        response = requests.get(trialEndpoint, headers=headers, params=payload)
+        response.raise_for_status()
+        sectionJson = response.json()
+        totalTrialsData.extend(sectionJson['data'])
+
+        if trialNum % 1000 == 0 and trialNum != 0:
+            with open(f'{trialNum-999}-{trialNum}Trials.json', 'w') as f:
+                json.dump(totalTrialsData, f)
+            totalTrialsData = []
+        time.sleep(4)
+
+    with open(f'{round(totalNumTrials, -3)}-{trialNum}Trials.json', 'w') as f:
+        json.dump(totalTrialsData, f)
+
+    end = time.perf_counter()
+    logger.debug(f'All trials queried in {end-start: .2f} seconds')
+
+
     # logger.debug(trialsResponse.json())
-    with open('response.json', 'w') as f:
-        json.dump(trialJson, f)
+    # with open('response.json', 'w') as f:
+    #     json.dump(trialJson, f)
 
 if __name__ == '__main__':
     main()
