a4163d6aae05baa4cbfa377ba735fe60ab0be54a
Jake Lucas <jak11772@esri.com>
2022-01-04 08:13:56 -0500

    adding eligibility and fixing max and min age data type

--- src/nciRetriever/__main__.py
+++ src/nciRetriever/__main__.py
@@ -6,6 +6,7 @@ from urllib.parse import urljoin
 import json
 import time
 import sys
+import os
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
@@ -31,41 +32,13 @@ def main():
     trialsResponse.raise_for_status()
     trialJson = trialsResponse.json()
     totalNumTrials = trialJson['total']
-    # finalFileNumTrials = totalNumTrials % 50
-# 
-    # totalTrialsData = []
-# 
-    # start = time.perf_counter()
-# 
-    # for trialNum in range(0, totalNumTrials, 50):
-        # logger.debug(trialNum)
-        # payload = {
-            # 'size': 50,
-            # 'from': trialNum
-        # }
-# 
-        # response = requests.get(trialEndpoint, headers=headers, params=payload)
-        # response.raise_for_status()
-        # sectionJson = response.json()
-        # totalTrialsData.extend(sectionJson['data'])
-# 
-        # if trialNum % 1000 == 0 and trialNum != 0:
-            # with open(f'{trialNum-999}-{trialNum}Trials.json', 'w') as f:
-                # json.dump(totalTrialsData, f)
-            # totalTrialsData = []
-        # time.sleep(4)
-# 
-    # with open(f'{round(totalNumTrials, -3)}-{trialNum}Trials.json', 'w') as f:
-        # json.dump(totalTrialsData, f)
-# 
-    # end = time.perf_counter()
-    # logger.debug(f'All trials queried in {end-start: .2f} seconds')
-# 
 
     start = time.perf_counter()
+    today = date.today()
 
     createdTrialCsv = False
     createdSiteCsv = False
+    createdEligibilityCsv = False
 
     for trialNumFrom in range(0, totalNumTrials, 50):
         sectionStart = time.perf_counter()
@@ -80,7 +53,11 @@ def main():
                                         'primaryPurpose', 
                                         'activeSitesCount', 
                                         'currentTrialStatus',
-                                        'startDate'])
+                                        'startDate',
+                                        'maxAgeInYears',
+                                        'minAgeInYears',
+                                        'gender',
+                                        'acceptsHealthyVolunteers'])
 
         sitesDf = pd.DataFrame(columns=['protocolId',
                                         'orgStateOrProvince',
@@ -103,6 +80,9 @@ def main():
                                         'orgName',
                                         'lat',
                                         'long'])
+        eligibilityDf = pd.DataFrame(columns=['protocolId',
+                                              'inclusionIndicator',
+                                              'description'])
         payload = {
             'size': 50,
             'from': trialNumFrom
@@ -126,51 +106,82 @@ def main():
                                     'startDate': trial['start_date']}
             if 'active_sites_count' in trial.keys():
                 trialDict.update({'activeSitesCount': trial['active_sites_count']})
+            if 'max_age_in_years' in trial['eligibility']['structured'].keys():
+                trialDict.update({'maxAgeInYears': int(trial['eligibility']['structured']['max_age_in_years'])})
+            if 'min_age_in_years' in trial['eligibility']['structured'].keys():
+                trialDict.update({'minAgeInYears': int(trial['eligibility']['structured']['min_age_in_years'])})
+            if 'gender' in trial['eligibility']['structured'].keys():
+                trialDict.update({'gender': trial['eligibility']['structured']['gender']})
+            if 'accepts_healthy_volunteers' in trial['eligibility']['structured'].keys():
+                trialDict.update({'acceptsHealthyVolunteers': trial['eligibility']['structured']['accepts_healthy_volunteers']})
             trialDf = pd.DataFrame.from_records([trialDict])
             trialsDf = pd.concat([trialsDf, trialDf], verify_integrity=True, ignore_index=True)
 
-            if trial['sites'] is None:
-                continue 
+            if trial['eligibility']['unstructured'] is not None:
+                for condition in trial['eligibility']['unstructured']:
+                    eligibilityDict = {
+                        'protocolId': trial['protocol_id'],
+                        'inclusionIndicator': condition['inclusion_indicator'],
+                        'description': condition['description']
+                    }
+                    conditionDf = pd.DataFrame.from_records([eligibilityDict])
+                    eligibilityDf = pd.concat([eligibilityDf, conditionDf], verify_integrity=True, ignore_index=True)
 
-            for site in trial['sites']:
-                siteDict = {'protocolId': trial['protocol_id'],
-                                    'orgStateOrProvince': site['org_state_or_province'],
-                                    'contactName': site['contact_name'],
-                                    'contactPhone': site['contact_phone'],
-                                    'recruitmentStatusDate': site['recruitment_status_date'],
-                                    'orgAddressLine1': site['org_address_line_1'],
-                                    'orgAddressLine2': site['org_address_line_2'],
-                                    'orgVa': site['org_va'],
-                                    'orgTty': site['org_tty'],
-                                    'orgFamily': site['org_family'],
-                                    'orgPostalCode': site['org_postal_code'],
-                                    'contactEmail': site['contact_email'],
-                                    'recruitmentStatus': site['recruitment_status'],
-                                    'orgCity': site['org_city'],
-                                    'orgEmail': site['org_email'],
-                                    'orgCountry': site['org_country'],
-                                    'orgFax': site['org_fax'],
-                                    'orgPhone': site['org_phone'],
-                                    'orgName': site['org_name'],
-                                    'lat': None,
-                                    'long': None}
-                if 'org_coordinates' in site.keys():
-                    siteDict['lat'] = site['org_coordinates']['lat']
-                    siteDict['long'] = site['org_coordinates']['lon']
-                siteDf = pd.DataFrame.from_records([siteDict])
-                
-                sitesDf = pd.concat([sitesDf, siteDf], ignore_index=True, verify_integrity=True)
+            if trial['sites'] is not None:
+
+                for site in trial['sites']:
+                    siteDict = {'protocolId': trial['protocol_id'],
+                                        'orgStateOrProvince': site['org_state_or_province'],
+                                        'contactName': site['contact_name'],
+                                        'contactPhone': site['contact_phone'],
+                                        'recruitmentStatusDate': site['recruitment_status_date'],
+                                        'orgAddressLine1': site['org_address_line_1'],
+                                        'orgAddressLine2': site['org_address_line_2'],
+                                        'orgVa': site['org_va'],
+                                        'orgTty': site['org_tty'],
+                                        'orgFamily': site['org_family'],
+                                        'orgPostalCode': site['org_postal_code'],
+                                        'contactEmail': site['contact_email'],
+                                        'recruitmentStatus': site['recruitment_status'],
+                                        'orgCity': site['org_city'],
+                                        'orgEmail': site['org_email'],
+                                        'orgCountry': site['org_country'],
+                                        'orgFax': site['org_fax'],
+                                        'orgPhone': site['org_phone'],
+                                        'orgName': site['org_name'],
+                                        'lat': None,
+                                        'long': None}
+                    if 'org_coordinates' in site.keys():
+                        siteDict['lat'] = site['org_coordinates']['lat']
+                        siteDict['long'] = site['org_coordinates']['lon']
+                    siteDf = pd.DataFrame.from_records([siteDict])
+                    
+                    sitesDf = pd.concat([sitesDf, siteDf], ignore_index=True, verify_integrity=True)
+
         if not createdSiteCsv:
-            sitesDf.to_csv(f'nciSites{date.today()}.csv', index=False)
+            if os.path.isfile(f'nciSites{today}.csv'):
+                os.remove(f'nciSites{today}.csv')
+            sitesDf.to_csv(f'nciSites{today}.csv', index=False)
             createdSiteCsv = True
         else:
-            sitesDf.to_csv(f'nciSites{date.today()}.csv', index=False, mode='a', header=False)
+            sitesDf.to_csv(f'nciSites{today}.csv', index=False, mode='a', header=False)
         
         if not createdTrialCsv:
-            trialsDf.to_csv(f'nciTrials{date.today()}.csv', index=False)
+            if os.path.isfile(f'nciTrials{today}.csv'):
+                os.remove(f'nciTrials{today}.csv')
+            trialsDf.to_csv(f'nciTrials{today}.csv', index=False)
             createdTrialCsv = True
         else:
-            trialsDf.to_csv(f'nciTrials{date.today()}.csv', index=False, mode='a', header=False)
+            trialsDf.to_csv(f'nciTrials{today}.csv', index=False, mode='a', header=False)
+
+        if not createdEligibilityCsv:
+            if os.path.isfile(f'nciEligibility{today}.csv'):
+                os.remove(f'nciEligibility{today}.csv')
+            eligibilityDf.to_csv(f'nciEligibility{today}.csv', index=False)
+            createdEligibilityCsv = True
+        else:
+            eligibilityDf.to_csv(f'nciEligibility{today}.csv', index=False, mode='a', header=False)
+
         sectionElapsed = time.perf_counter()-sectionStart
 
         logger.debug(f'Trials {trialNumFrom}-{trialNumFrom+50} retrieved in {sectionElapsed: .2f}s')
